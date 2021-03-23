cc0b0440e75eb6bcbb03b3471a8468ff8e9e00fe
1543rrr <73112533+1543rrr@users.noreply.github.com>
2021-03-23 22:30:33 +0900

    create_show_page

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,9 +2,28 @@ class HomesController < ApplicationController
   before_action :set_home, only: [:edit, :show]
 
   def index
+    @homes = Home.all
+  end
+
+  def new
+    @home = Home.new
+  end
+
+  def create
+    Home.create(home_params)
+  end
+
+  def destroy
+    home = Home.find(params[:id])
+    home.destroy
   end
 
   def edit
+  end
+
+  def update
+    home = Home.find(params[:id])
+    home.update(home_params)
   end
 
   def show
