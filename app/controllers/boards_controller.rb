fb2832f0a6e214e40c22b7dc5fb08c26eeb8d3cf
1543rrr <73112533+1543rrr@users.noreply.github.com>
2021-03-23 23:27:59 +0900

    create_show_action

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,31 @@
+class BoardsController < ApplicationController
+  before_action :authenticate_user!, only: :new
+  def index
+    @boards = Board.all
+  end
+
+  def show
+    @board = Board.find(params[:id])
+  end
+
+  def new
+    @board = current_user.boards.new
+  end
+
+  def create
+    board = current_user.boards.new(board_params)
+    if board.save
+      flash[:notice] = "投稿が完了しました。"
+      redirect_to boards_path
+    else
+      flash[:error_messages] = board.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+    def board_params
+      params.permit(:content, :user_id)
+    end
+end
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,41 +1,4 @@
 class HomesController < ApplicationController
-  before_action :set_home, only: [:edit, :show]
-
   def index
-    @homes = Home.all
-  end
-
-  def new
-    @home = Home.new
-  end
-
-  def create
-    Home.create(home_params)
-  end
-
-  def destroy
-    home = Home.find(params[:id])
-    home.destroy
-  end
-
-  def edit
-  end
-
-  def update
-    home = Home.find(params[:id])
-    home.update(home_params)
-  end
-
-  def show
-  end
-
-  private
-
-  def home_params
-    params.permit(:name, :text)
-  end
-
-  def set_tweet
-    @home = Home.find(params[:id])
   end
 end
