9d7e5076814bd754979fdbd5a5867e6764a3e996
Yuki Ishizaki <ishiyuki405@icloud.com>
2021-05-08 18:48:23 +0900

    LikesControllerの定義完了

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,16 @@
+class LikesController < ApplicationController
+  before_action :board_params
+
+  def create
+    current_user.likes.create(board_id: @board.id)
+  end
+
+  def destroy
+    current_user.likes.find_by(board_id: @board.id)
+  end
+
+  private
+    def board_params
+      @board = Board.find(params[:board_id])
+    end
+end
