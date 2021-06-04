9f8eb23c455c6a9b80eb8d727f648eb2f50a198d
pigu14 <tigersyamato062@gmail.com>
2021-06-04 22:27:20 +0900

    コメントのエラーメッセージの日本語化

--- db/schema.rb
+++ db/schema.rb
@@ -24,13 +24,11 @@ ActiveRecord::Schema.define(version: 2021_05_08_092646) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string "name"
     t.text "comment"
-    t.text "board"
-    t.bigint "user_id", null: false
-    t.bigint "board_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.bigint "board_id", null: false
     t.index ["board_id"], name: "index_comments_on_board_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
